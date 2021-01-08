3547ee66b330af386b6cef03455388c00e87d30f
Atziri <atziripg.99@gmail.com>
2021-01-08 12:57:17 -0600

    entity en entidades

--- proyectomvc-APG/src/main/java/com/ipn/mx/modelo/entidades/Pelicula.java
+++ proyectomvc-APG/src/main/java/com/ipn/mx/modelo/entidades/Pelicula.java
@@ -1,6 +1,11 @@
 package com.ipn.mx.modelo.entidades;
 
 import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -12,7 +17,11 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Entity
+@Table(name = "Pelicula",schema = "public")
 public class Pelicula implements Serializable{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int idPelicula;
     private int idGenero;
     private String nombrePelicula;
--- proyectomvc-APG/src/main/java/com/ipn/mx/modelo/entidades/Usuario.java
+++ proyectomvc-APG/src/main/java/com/ipn/mx/modelo/entidades/Usuario.java
@@ -6,6 +6,11 @@
 package com.ipn.mx.modelo.entidades;
 
 import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -17,7 +22,11 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Entity
+@Table(name = "Usuario",schema = "public")
 public class Usuario implements Serializable{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int idUsuario;
     private String nombre;
     private String paterno;
