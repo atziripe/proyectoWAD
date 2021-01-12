2ad19fb9c77e29a302a8409ab16294dc78ea8abb
Fernando_HrdzE <60621462+FHE9317@users.noreply.github.com>
2021-01-11 21:18:34 -0600

    Agregando opciones

--- daleGIT.bat
+++ daleGIT.bat
@@ -1,7 +1,25 @@
 @echo off
+echo 1. PULL
+echo 2. PUSH
+echo 3. Salir
+echo.
+set/p op= Elija una opcion:
+if %op%== 1 (goto :1)
+if %op%== 2 (goto :2)
+if %op%== 3 (goto :3)
+
+:1
+cls
+git pull origin main
+pause
+exit
+
+:2
+cls
 set/p name= El commit:
 git pull origin main
 git add .
 git commit -m %name%
 git push origin main
 pause
+exit
