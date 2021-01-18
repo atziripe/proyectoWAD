e0b3309dd66a41d87282a49a1840b3efc3ca2aec
Atziri <atziripg.99@gmail.com>
2021-01-18 14:35:43 -0600

    No se que estoy haciendo

--- proyectomvc-APG/src/main/java/com/ipn/mx/controlador/web/GeneroMB.java
+++ proyectomvc-APG/src/main/java/com/ipn/mx/controlador/web/GeneroMB.java
@@ -90,7 +90,7 @@ public class GeneroMB extends BaseBean implements Serializable{
         return prepareIndex();
     }
     
-    public void seleccionarCateoria(ActionEvent event){
+    public void seleccionarCategoria(ActionEvent event){
         String claveSel = (String) 
                 FacesContext.getCurrentInstance().
                         getExternalContext().
